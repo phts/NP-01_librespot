4f63795715ac75437670773e75d3af100b7fd30f
ashthespy <ashthespy@gmail.com>
2019-03-12 17:18:29 +0100

    Improve playback speed
    Credits to #297(kaymes:kaymes-faster)

--- playback/src/player.rs
+++ playback/src/player.rs
@@ -540,14 +540,12 @@ impl PlayerInternal {
             }
         };
 
-        let key = self
-            .session
-            .audio_key()
-            .request(track.id, file_id)
-            .wait()
-            .unwrap();
+        // Request in parallel
+        let key = self.session.audio_key().request(track.id, file_id);
+        let encrypted_file = AudioFile::open(&self.session, file_id);
 
-        let encrypted_file = AudioFile::open(&self.session, file_id).wait().unwrap();
+        let encrypted_file = encrypted_file.wait().unwrap();
+        let key = key.wait().unwrap();
 
         let mut decrypted_file = AudioDecrypt::new(key, encrypted_file);
 
@@ -563,11 +561,13 @@ impl PlayerInternal {
 
         let mut decoder = VorbisDecoder::new(audio_file).unwrap();
 
-        match decoder.seek(position) {
-            Ok(_) => (),
-            Err(err) => error!("Vorbis error: {:?}", err),
+        // Seek only if required
+        if position != 0 {
+            match decoder.seek(position) {
+                Ok(_) => (),
+                Err(err) => error!("Vorbis error: {:?}", err),
+            }
         }
-
         info!("Track \"{}\" loaded", track.name);
 
         Some((decoder, normalisation_factor))
