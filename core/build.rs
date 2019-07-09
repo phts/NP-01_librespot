dcff5c1850405a2d3964caa22de46b70772a98d9
ashthespy <ashthespy@gmail.com>
2019-07-09 18:32:04 +0200

    Bump up vergen (and switch to rustc-env flags)

--- core/build.rs
+++ core/build.rs
@@ -3,38 +3,17 @@ extern crate vergen;
 
 use rand::distributions::Alphanumeric;
 use rand::Rng;
-use std::env;
-use std::fs::OpenOptions;
-use std::io::Write;
-use std::path::PathBuf;
+use vergen::{generate_cargo_keys, ConstantsFlags};
 
 fn main() {
-    let out = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut flags = ConstantsFlags::all();
+    flags.toggle(ConstantsFlags::REBUILD_ON_HEAD_CHANGE);
+    generate_cargo_keys(ConstantsFlags::all()).expect("Unable to generate the cargo keys!");
 
-    vergen::vergen(vergen::OutputFns::all()).unwrap();
     let mut rng = rand::thread_rng();
     let build_id: String = ::std::iter::repeat(())
         .map(|()| rng.sample(Alphanumeric))
         .take(8)
         .collect();
-
-    let mut version_file = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(&out.join("version.rs"))
-        .unwrap();
-
-    let build_id_fn = format!(
-        "
-/// Generate a random build id.
-pub fn build_id() -> &'static str {{
-    \"{}\"
-}}
-",
-        build_id
-    );
-
-    if let Err(e) = version_file.write_all(build_id_fn.as_bytes()) {
-        println!("{}", e);
-    }
+    println!("cargo:rustc-env=VERGEN_BUILD_ID={}", build_id);
 }
