8f9aca710d9efb6d93bffdca841be7d13c9cc96e
Sasha Hilton <sashahilton00@users.noreply.github.com>
2019-11-12 10:46:21 +0100

    Merge pull request #393 from kaymes/dynamic-blocks
    Downloading files with dynamic block sizes for faster seek and playback

--- audio/src/lib.rs
+++ audio/src/lib.rs
@@ -6,6 +6,7 @@ extern crate log;
 extern crate aes_ctr;
 extern crate bit_set;
 extern crate byteorder;
+extern crate bytes;
 extern crate num_bigint;
 extern crate num_traits;
 extern crate tempfile;
@@ -20,8 +21,14 @@ mod lewton_decoder;
 #[cfg(any(feature = "with-tremor", feature = "with-vorbis"))]
 mod libvorbis_decoder;
 
+mod range_set;
+
 pub use decrypt::AudioDecrypt;
-pub use fetch::{AudioFile, AudioFileOpen};
+pub use fetch::{AudioFile, AudioFileOpen, StreamLoaderController};
+pub use fetch::{
+    READ_AHEAD_BEFORE_PLAYBACK_ROUNDTRIPS, READ_AHEAD_BEFORE_PLAYBACK_SECONDS,
+    READ_AHEAD_DURING_PLAYBACK_ROUNDTRIPS, READ_AHEAD_DURING_PLAYBACK_SECONDS,
+};
 
 #[cfg(not(any(feature = "with-tremor", feature = "with-vorbis")))]
 pub use lewton_decoder::{VorbisDecoder, VorbisError, VorbisPacket};
